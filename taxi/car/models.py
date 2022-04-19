d7e5e9f6fae502b49497772c2e03a0aa16a6fd13
Timoshka996 <abylkasymovtim23@gmail.com>
2022-04-19 21:28:21 +0600

    Taxi

---
+++ taxi/car/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Email(models.Model):
+    pochta = models.EmailField(blank=True, null=True)
+
+
+class Join(models.Model):
+    car_type = models.CharField(max_length=250)
+    car_tip = models.CharField(max_length=250)
+    car_size = models.CharField(max_length=250)
+
+    def __str__(self):
+        return f"{self.car_tip}, {self.car_type}, {self.car_size}"
